aeb83e903040c63a5a0de3900cfdb8e275d8a7ff
MassimilianoRocco <massimilianorocco93@gmail.com>
2024-04-19 16:56:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let x;
+for(x=1; x<=100; x++){
+
+    if(x%3==0){
+        const para = document.createElement("li");
+        const node = document.createTextNode("Fizz")
+        para.appendChild(node);
+        const element = document.getElementById("prova");
+        element.appendChild(para);
+        console.log("Fizz");
+    }
+    if(x % 5 == 0) {
+        const para = document.createElement("li");
+        const node = document.createTextNode("Buzz")
+        para.appendChild(node);
+        const element = document.getElementById("prova");
+        element.appendChild(para);
+        console.log("Buzz");
+    }
+    if((x % 3 == 0) && (x % 5 == 0)){
+        const para = document.createElement("li");
+        const node = document.createTextNode("FizzBuzz")
+        para.appendChild(node);
+        const element = document.getElementById("prova");
+        element.appendChild(para);
+        console.log("FizzBuzz");
+    }
+}
